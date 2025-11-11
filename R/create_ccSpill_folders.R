f4a41a38fdd7cf9741b813b992468c23e069336f
Kris Elbein <elbei001@umn.edu>
2025-11-11 14:28:15 -0600

    Updated template

--- R/create_ccSpill_folders.R
+++ R/create_ccSpill_folders.R
@@ -15,7 +15,7 @@ create_ccSpill_folders <- function(path) {
 
   # 1. Find the path to the template folder inside your installed package
   template_path <- system.file(
-    "templates", "my_project_template",
+    "data",
     package = "ccSpill"
   )
 
